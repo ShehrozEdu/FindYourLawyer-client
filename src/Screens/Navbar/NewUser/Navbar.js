30e27c9d0146d2abdcc8feae7250cf55db91b13d
Shehroz Batt <101613341+ShehrozEdu@users.noreply.github.com>
2022-09-16 14:13:30 +0530

    Update Navbar.js

--- src/Screens/Navbar/NewUser/Navbar.js
+++ src/Screens/Navbar/NewUser/Navbar.js
@@ -108,10 +108,10 @@ export default function NewUSerNavBar() {
         <div className="justify-between px-4 mx-auto lg:max-w-7xl md:items-center md:flex md:px-8">
           <div>
             <div className="flex items-center justify-between py-3 md:py-5 md:block">
-              <Link to="/" className="m-0 p-0 ">
-                <img src="/img/logo.png" className="logo m-0 p-0 " alt="" />
+              <Link to="/" className="mr-0 ">
+                <img src="/img/logo.png" className="logo mr-0" alt="" />
               </Link>
-              <h2 className="ml-3 text-xl text-black dark:text-white">
+              <h2 className="ml-3 text-sm text-black dark:text-white">
                 Find Your Lawyer
               </h2>
               <div className="md:hidden">
